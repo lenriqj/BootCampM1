b9e2a56be5b900b983cc4b9eaedd5123e604b354
johanclavijo <joah1903@gmail.com>
2023-02-13 16:02:02 -0500

    13 Febrero 2023

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -1,8 +1,44 @@
 'use strict';
 
-function BinarioADecimal(num) {}
+function BinarioADecimal(num) {
+  var arrNumeros=[];
+  var pow=num.length -1;
+  for(let i of num){
+    arrNumeros.push(
+      Number(i) * 2 ** pow
+    )
+    pow--;
+  }
+  var decimal=0;
+  for(let i of arrNumeros){
+    decimal += i;
+  }
 
-function DecimalABinario(num) {}
+  return decimal;
+
+  
+  //return parseInt(num, 2);
+   
+}
+
+
+  
+
+
+
+function DecimalABinario(num) {
+  var arr=[];
+  var decimal=Number(num);
+ while(decimal !== 0){
+ arr.push(decimal % 2);
+ decimal = Math.floor(decimal / 2);
+  
+  
+ }
+ return arr.reverse().join("");
+
+}
+console.log(DecimalABinario("11"));
 
 module.exports = {
    BinarioADecimal,
